65f7a0fbccd9dd02839bac7701ee3749369f39a1
Milena Silva <ravlismilena@gmail.com>
2026-02-07 20:13:30 -0300

    chore: init

---
+++ src/main/java/projeto/manutencao_embarcacoes/ManutencaoEmbarcacoesApplication.java
@@ -0,0 +1,13 @@
+package projeto.manutencao_embarcacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManutencaoEmbarcacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManutencaoEmbarcacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projeto/manutencao_embarcacoes/ManutencaoEmbarcacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.manutencao_embarcacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManutencaoEmbarcacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
